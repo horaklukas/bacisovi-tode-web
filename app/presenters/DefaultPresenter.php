e9247a3727b73a2afd326d51a5296173dbf8625f
Lukas Horak <horaklukas@centrum.cz>
2016-03-27 10:36:52 +0200

    Add some initial intro page content

--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -3,7 +3,30 @@
 //  namespace App;
 
 class DefaultPresenter extends Nette\Application\UI\Presenter {
+  public function renderDefault() {
+    $this->template->code = '
+      (function() {
+        var web, pageContent;
 
+        web = createWebsite("bacisovi.tode.cz");
+
+        for(var i = 0; i < contentPages.length; i++) {
+          pageContent = contentPages[i];
+
+          developer.addContentPage(function(pageTarget) {
+            renderPage(pageTarget, pageContent)
+          });
+        }
+
+        function renderPage(element, content) {
+          if(typeof content !== "string") {
+            content = serialize(content);
+          }
+
+          element.innerHTML = content;
+        }
+      })()';
+  }
 }
 
 ?>
